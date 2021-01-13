980a6365002fa6ac58315ea92e0cfaa8442aff31
Joe Spraggon <joespraggon@Joes-MacBook-Air.local>
2021-01-13 15:18:10 +0000

    step 6 complete

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,13 @@
 class Oystercard
     attr_reader :balance
+    MAX_BALANCE = 90
 
-    def initialize
-        @balance = 0
+    def initialize(balance=0)
+        @balance = balance
     end
 
     def top_up(amount)
+        raise "Top up limit reached" if balance + amount > MAX_BALANCE
         @balance += amount
     end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,32 +1,30 @@
 require 'oystercard'
 
 describe Oystercard do
+    let(:topped_up_card) {Oystercard.new(20)}
+    subject{describe_class.new}
     it 'has a default balance of zero' do
         expect(subject.balance).to eq 0
     end
 
-    it 'increases balance by 5 when we top_up 5' do
-        card = Oystercard.new
-        card.top_up(5)
-        expect(card.balance).to eq 5
+    it 'increases balance by 20 when we top_up 20' do
+        expect(topped_up_card.balance).to eq 20
     end 
 
-    it 'increases balance by 10 when we top_up 10' do
-        card = Oystercard.new
-        card.top_up(10)
-        expect(card.balance).to eq 10
+    it 'increases balance by 30 when we top_up 30' do
+        topped_up_card.top_up(10)
+        expect(topped_up_card.balance).to eq 30
     end
 
-    it 'increases balance by 15 when we top_up 10 & 5' do
-        card = Oystercard.new
-        card.top_up(10)
-        card.top_up(5)
-        expect(card.balance).to eq 15
+    it 'increases balance by 35 when we top_up 10 & 5' do
+        topped_up_card.top_up(10)
+        topped_up_card.top_up(5)
+        expect(topped_up_card.balance).to eq 35
     end
 
     it 'has a balance limit of 90' do
-        card = Oystercard.new
-        card.top_up
+        topped_up_card.top_up(70)
+        expect{topped_up_card.top_up(1)}.to raise_error "Top up limit reached"
     end
 
 
