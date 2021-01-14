a5b2f5c2002fbfd373c08deb6cada866deabde8e
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-01-14 15:27:13 +0000

    step 11 complete, card now stores entry station

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,12 @@
 class Oystercard
-    attr_reader :balance, :journey
-    MAX_BALANCE = 90
+    attr_reader :balance, :entry_station
+    MAX_BALANCE = 
     MIN_FARE = 1
     DEFAULT_BALANCE = 0
 
     def initialize(balance= DEFAULT_BALANCE)
         @balance = balance
-        @journey = false
+        @entry_station = nil
     end
 
     def top_up(amount)
@@ -15,17 +15,17 @@ class Oystercard
     end
 
     def in_journey?
-      @journey
+      !(@entry_station == nil)
     end
 
-    def touch_in
+    def touch_in(station)
       fail "Insufficent funds" if balance < MIN_FARE
-      @journey = true
+      @entry_station = station
     end
 
     def touch_out
       deduct(MIN_FARE)  
-      @journey = false
+      @entry_station = nil
     end
 
     private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,6 +3,7 @@ require 'oystercard'
 describe Oystercard do
   subject { described_class.new }
   let(:topped_up_card) { Oystercard.new(Oystercard::MAX_BALANCE) }
+  let(:station) { double("oxford_street") }
 
     it 'has a default balance of zero' do
         expect(subject.balance).to eq Oystercard::DEFAULT_BALANCE
@@ -24,7 +25,7 @@ describe Oystercard do
     end
 
     it 'start our journey' do
-        topped_up_card.touch_in
+        topped_up_card.touch_in(station)
         expect(topped_up_card).to be_in_journey
     end
 
@@ -34,13 +35,21 @@ describe Oystercard do
     end
 
     it 'raises an error when insufficent funds' do
-      expect{subject.touch_in}.to raise_error "Insufficent funds"
+      expect{subject.touch_in(station)}.to raise_error "Insufficent funds"
     end
 
     it 'charges for a journey when we touch out' do
-        topped_up_card.touch_in
+        topped_up_card.touch_in(station)
         expect{topped_up_card.touch_out}.to change{topped_up_card.balance}.by(-Oystercard::MIN_FARE)
     end
 
+    it 'touch_in method accepts an argument' do
+        expect(topped_up_card).to respond_to(:touch_in).with(1).argument
+    end
+
+    it 'touch_in method stores station name on card' do
+        station = double("oxford_street")
+        expect{topped_up_card.touch_in(station)}.to change{topped_up_card.entry_station}.to(station)
+    end
 
 end
